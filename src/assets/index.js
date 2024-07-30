343fda2665aaa97986635444806a040c7f8cc27f
sohaib chebah <sohaib.chebah2020@gmail.com>
2024-07-30 21:11:47 +0100

    main: my first commit

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,40 +1,40 @@
 import logo from "./logo.svg";
 import backend from "./backend.png";
-import creator from "./creator.png";
-import mobile from "./mobile.png";
+
+
 import web from "./web.png";
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
 
 import css from "./tech/css.png";
+import nextjs from "./tech/nextjs.svg";
 import docker from "./tech/docker.png";
 import figma from "./tech/figma.png";
 import git from "./tech/git.png";
 import html from "./tech/html.png";
 import javascript from "./tech/javascript.png";
 import mongodb from "./tech/mongodb.png";
-import nodejs from "./tech/nodejs.png";
 import reactjs from "./tech/reactjs.png";
 import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+
+import esi from "./company/esi.png";
+
+
 
 import carrent from "./carrent.png";
-import jobit from "./jobit.png";
-import tripguide from "./tripguide.png";
+import prompt from "./prompt.png";
+import leon from "./leon.png";
+
 
 export {
   logo,
   backend,
-  creator,
-  mobile,
+  
   web,
   github,
   menu,
@@ -45,18 +45,20 @@ export {
   git,
   html,
   javascript,
+  nextjs,
+ 
   mongodb,
-  nodejs,
+  
   reactjs,
   redux,
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
+  
+  esi,
+  
   carrent,
-  jobit,
-  tripguide,
+  
+  prompt,
+  leon
 };
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,27 +1,30 @@
 import {
-  mobile,
+  
   backend,
-  creator,
+  
   web,
   javascript,
+  nextjs,
+  
   typescript,
   html,
   css,
   reactjs,
   redux,
   tailwind,
-  nodejs,
+  
   mongodb,
   git,
   figma,
   docker,
-  meta,
-  starbucks,
-  tesla,
-  shopify,
+  
+  esi,
+  
   carrent,
-  jobit,
-  tripguide,
+  
+  prompt,
+  leon,
+  
   threejs,
 } from "../assets";
 
@@ -45,18 +48,12 @@ const services = [
     title: "Web Developer",
     icon: web,
   },
-  {
-    title: "React Native Developer",
-    icon: mobile,
-  },
+  
   {
     title: "Backend Developer",
     icon: backend,
   },
-  {
-    title: "Content Creator",
-    icon: creator,
-  },
+  
 ];
 
 const technologies = [
@@ -81,17 +78,19 @@ const technologies = [
     icon: reactjs,
   },
   {
+    name: "Next JS",
+    icon: nextjs,
+  },
+  {
     name: "Redux Toolkit",
     icon: redux,
   },
+  
   {
     name: "Tailwind CSS",
     icon: tailwind,
   },
-  {
-    name: "Node JS",
-    icon: nodejs,
-  },
+  
   {
     name: "MongoDB",
     icon: mongodb,
@@ -117,51 +116,26 @@ const technologies = [
 const experiences = [
   {
     title: "React.js Developer",
-    company_name: "Starbucks",
-    icon: starbucks,
+    company_name: "Esi sba",
+    icon: esi,
     iconBg: "#383E56",
-    date: "March 2020 - April 2021",
+    date: "March 2024 - May 2024",
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Developing and maintaining web application using React.js and other related technologies.",
       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
       "Implementing responsive design and ensuring cross-browser compatibility.",
       "Participating in code reviews and providing constructive feedback to other developers.",
     ],
   },
   {
-    title: "React Native Developer",
-    company_name: "Tesla",
-    icon: tesla,
+    title: "JavaScript Developer",
+    company_name: "Esi sba",
+    icon: esi,
     iconBg: "#E6DEDD",
-    date: "Jan 2021 - Feb 2022",
+    date: "OCT 2023",
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
-    ],
-  },
-  {
-    title: "Web Developer",
-    company_name: "Shopify",
-    icon: shopify,
-    iconBg: "#383E56",
-    date: "Jan 2022 - Jan 2023",
-    points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
-    ],
-  },
-  {
-    title: "Full stack Developer",
-    company_name: "Meta",
-    icon: meta,
-    iconBg: "#E6DEDD",
-    date: "Jan 2023 - Present",
-    points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
+      "kodikos hackathon ...",
+      "Developing and maintaining web application using html css and javascript.",
       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
       "Implementing responsive design and ensuring cross-browser compatibility.",
       "Participating in code reviews and providing constructive feedback to other developers.",
@@ -172,26 +146,26 @@ const experiences = [
 const testimonials = [
   {
     testimonial:
-      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-    name: "Sara Lee",
-    designation: "CFO",
-    company: "Acme Co",
+      "I thought it was impossible to make a website as beautiful as our product, but Sohaib proved me wrong.",
+    name: "Yacine Djaaraoui",
+    designation: "Student",
+    company: "esi sba",
     image: "https://randomuser.me/api/portraits/women/4.jpg",
   },
   {
     testimonial:
-      "I've never met a web developer who truly cares about their clients' success like Rick does.",
-    name: "Chris Brown",
-    designation: "COO",
-    company: "DEF Corp",
+      "I've never met a web developer who truly cares about their clients' success like Sohaib does.",
+    name: "Djawad Boufalghed",
+    designation: "Student",
+    company: "esi sba",
     image: "https://randomuser.me/api/portraits/men/5.jpg",
   },
   {
     testimonial:
-      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-    name: "Lisa Wang",
-    designation: "CTO",
-    company: "456 Enterprises",
+      "After Sohaib optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Walid Benchikh",
+    designation: "Student",
+    company: "esi sba",
     image: "https://randomuser.me/api/portraits/women/6.jpg",
   },
 ];
@@ -216,49 +190,45 @@ const projects = [
       },
     ],
     image: carrent,
-    source_code_link: "https://github.com/",
+    source_code_link: "https://github.com/sohaibchebah/Lotok",
   },
   {
-    name: "Job IT",
+    name: "Prompting tool",
     description:
-      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
-    tags: [
-      {
-        name: "react",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "restapi",
-        color: "green-text-gradient",
-      },
-      {
-        name: "scss",
-        color: "pink-text-gradient",
-      },
-    ],
-    image: jobit,
-    source_code_link: "https://github.com/",
-  },
-  {
-    name: "Trip Guide",
-    description:
-      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+      "Web application that enables users to search for prompts, Promptopia is an open-source AI prompting tool for modern world to discover, create and share creative prompts",
     tags: [
       {
         name: "nextjs",
         color: "blue-text-gradient",
       },
       {
-        name: "supabase",
+        name: "mongodb",
         color: "green-text-gradient",
       },
+      {
+        name: "tailwindcss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: prompt,
+    source_code_link: "https://github.com/sohaibchebah/PromptHelp",
+  },
+  {
+    name: "Leon template",
+    description: "leon template using html css",
+    tags: [
+      {
+        name: "html",
+        color: "blue-text-gradient",
+      },
+
       {
         name: "css",
         color: "pink-text-gradient",
       },
     ],
-    image: tripguide,
-    source_code_link: "https://github.com/",
+    image: leon,
+    source_code_link: "https://github.com/sohaibchebah/web-project",
   },
 ];
 
