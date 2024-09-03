2cf6e8dfb8c147c8629a415d49349bcc9804af11
sohaib chebah <sohaib.chebah2020@gmail.com>
2024-09-03 15:14:52 +0100

    fix email js

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -36,10 +36,9 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
-          from_name: form.name,
-          to_name: "Sohaib Chebah",
-          from_email: form.email,
-          to_email: "sohaib.chebah2020@gmail.com",
+          name: form.name,
+          email: form.email,
+          
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
